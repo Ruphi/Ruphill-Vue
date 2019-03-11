e5760b17787728394107ec15e4d5c0315fa392a5
ruphi <morrisfan@126.com>
2019-03-11 23:22:25 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@
             <div class="card-img-text" style="margin-right: 30px"><h2 class="second"> 前進する</h2></div>
             <div class="card-img-text"><h2 class="first">一颗橡皮</h2></div>
             <v-container style="float: right">
-              <v-card-title primary-title style="float: right;border: 1px solid #26A69A;margin-top: 20vh;margin-right: 30px;box-shadow: 0 3px 1px -2px #00695C, 0 2px 2px 0 #4DB6AC, 0 1px 5px 0 #B2DFDB;border-radius: 2px;">
+              <v-card-title primary-title>
                 <div style="text-align: left; margin-top: 10vh;">
                   <div style="padding: 8px; color: #009688">
                     <h3 class="headline mb-0">生活，</h3>
@@ -53,7 +53,7 @@
     float: right;
   }
   .card-img-text .first{
-    margin-top: 100px;
+    margin-top: 80px;
   }
   .content{
     height: 100vh;
@@ -63,5 +63,15 @@
   }
   .container.grid-list-md .layout:only-child{
     margin: 0;
+  }
+  .v-card__title{
+    position: absolute;
+    float: right;
+    border: 1px solid rgb(38, 166, 154);
+    box-shadow: rgb(0, 105, 92) 0px 3px 1px -2px, rgb(77, 182, 172) 0px 2px 2px 0px, rgb(178, 223, 219) 0px 1px 5px 0px;
+    border-radius: 2px;
+    right: 38px;
+    bottom: 10px;
+    background: rgba(255,255,255,0.6)
   }
 </style>
